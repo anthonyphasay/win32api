59962bbe6291135927bea1f39cee99f5b6996d34
anthonyphasay <72152681+anthonyphasay@users.noreply.github.com>
2025-03-28 12:58:04 -0600

    Initial window

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <Windows.h>
+
+LRESULT WinProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstanc, PSTR lpCmdLine, int nCmdShow)
+{
+    WNDCLASSA class = {
+        0,
+    }
+    return 0;
+}
